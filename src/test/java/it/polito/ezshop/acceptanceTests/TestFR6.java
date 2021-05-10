d201fa66df0b7311d0dca658e251b4e727505dc3
Marzio <marziovallero@gmail.com>
2021-05-10 15:52:31 +0200

    Implemented testApplyDiscountRateToSale(), testComputePointsForSale()

--- src/test/java/it/polito/ezshop/acceptanceTests/TestFR6.java
+++ src/test/java/it/polito/ezshop/acceptanceTests/TestFR6.java
@@ -306,4 +306,130 @@ public class TestFR6 {
 		
 		return;
 	}
+	
+	@Test
+	public void testApplyDiscountRateToSale() throws InvalidUsernameException, InvalidPasswordException, InvalidRoleException, InvalidTransactionIdException, InvalidDiscountRateException, UnauthorizedException {
+		EZShopInterface EzShop = new EZShop();
+		Integer id = 0;
+		User usr;
+		boolean ret = false;
+		
+		try {
+			ret = EzShop.applyDiscountRateToSale(id, 0.1);
+			fail();
+		} catch (UnauthorizedException e) {
+			
+		}
+		
+		id = EzShop.createUser("Marzio", "password", "Administrator");
+		assertTrue(id > 0);
+		usr = EzShop.login("Marzio", "password");
+		assertTrue(usr != null);
+		
+		id = EzShop.startSaleTransaction();
+		assertTrue(id > 0);
+		
+		try {
+			ret = EzShop.applyDiscountRateToSale(null, 0.1);
+			fail();
+		} catch (InvalidTransactionIdException e) {
+			
+		}
+		
+		try {
+			ret = EzShop.applyDiscountRateToSale(0, 0.1);
+			fail();
+		} catch (InvalidTransactionIdException e) {
+			
+		}
+		
+		try {
+			ret = EzShop.applyDiscountRateToSale(id, -0.1);
+			fail();
+		} catch (InvalidDiscountRateException e) {
+			
+		}
+		
+		try {
+			ret = EzShop.applyDiscountRateToSale(id, 1.1);
+			fail();
+		} catch (InvalidDiscountRateException e) {
+			
+		}
+		
+		ret = EzShop.applyDiscountRateToSale(id+1, 0.1);
+		assertTrue(!ret);
+		
+		ret = EzShop.applyDiscountRateToSale(id, 0.1);
+		assertTrue(ret);
+		
+		return;
+	}
+	
+	@Test
+	public void testComputePointsForSale() throws InvalidTransactionIdException, InvalidUsernameException, InvalidPasswordException, InvalidRoleException, InvalidProductDescriptionException, InvalidProductCodeException, InvalidPricePerUnitException, UnauthorizedException, InvalidQuantityException, InvalidProductIdException, InvalidPaymentException {
+		EZShopInterface EzShop = new EZShop();
+		Integer id = 0, points = 0;
+		double change = 0;
+		User usr;
+		boolean ret = false;
+		
+		try {
+			points = EzShop.computePointsForSale(id);
+			fail();
+		} catch (UnauthorizedException e) {
+			
+		}
+		
+		id = EzShop.createUser("Marzio", "password", "Administrator");
+		assertTrue(id > 0);
+		usr = EzShop.login("Marzio", "password");
+		assertTrue(usr != null);
+		
+		id = EzShop.createProductType("NieR Piano Collections", "122474487139", 10.0, "Music CD");
+		assertTrue(id != -1);
+		
+		ret = EzShop.updateQuantity(id, 2);
+		assertTrue(ret);
+		
+		id = EzShop.startSaleTransaction();
+		assertTrue(id > 0);
+		
+		ret = EzShop.addProductToSale(id, "122474487139", 1);
+		assertTrue(ret);
+		
+		try {
+			points = EzShop.computePointsForSale(null);
+			fail();
+		} catch (InvalidTransactionIdException e) {
+			
+		}
+		
+		try {
+			points = EzShop.computePointsForSale(0);
+			fail();
+		} catch (InvalidTransactionIdException e) {
+			
+		}
+		
+		points = EzShop.computePointsForSale(id+1);
+		assertTrue(points == -1);
+		
+		points = EzShop.computePointsForSale(id);
+		assertTrue(points == 1);
+		
+		ret = EzShop.endSaleTransaction(id);
+		assertTrue(ret);
+		points = EzShop.computePointsForSale(id);
+		assertTrue(points == 1);
+		
+		change = EzShop.receiveCashPayment(id, 11.0);
+		assertTrue(change == 1);
+		points = EzShop.computePointsForSale(id);
+		assertTrue(points == 1);
+		
+		return;
+	}
+	
+	
 }

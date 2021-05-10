f60029c8797a1537529b9d361458f4e8d470ceaf
Marzio <marziovallero@gmail.com>
2021-05-10 11:24:59 +0200

    Implemented testGetCustomer()

--- src/test/java/it/polito/ezshop/acceptanceTests/TestFR5.java
+++ src/test/java/it/polito/ezshop/acceptanceTests/TestFR5.java
@@ -1,5 +1,54 @@
 package it.polito.ezshop.acceptanceTests;
 
-public class TestFR5 {
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import org.junit.Test;
+import it.polito.ezshop.data.*;
+import it.polito.ezshop.exceptions.*;
+
+public class TestFR5 {
+	
+	@Test
+	public void testGetCustomer() throws InvalidCustomerIdException, UnauthorizedException, InvalidUsernameException, InvalidPasswordException, InvalidRoleException, InvalidCustomerNameException {
+		EZShopInterface EzShop = new EZShop();
+		Customer cus1 = null;
+		Integer id = 0;
+		
+		try {
+			cus1 = EzShop.getCustomer(10);
+			fail();
+		} catch (UnauthorizedException e) {
+			
+		}		
+		
+		id = EzShop.createUser("Marzio", "password", "ADMIN");
+		assertTrue(id > 0);
+		
+		
+		try {
+			cus1 = EzShop.getCustomer(null);
+			fail();
+		} catch (InvalidCustomerIdException e) {
+			
+		}
+		
+		try {
+			cus1 = EzShop.getCustomer(0);
+			fail();
+		} catch (InvalidCustomerIdException e) {
+			
+		}
+		
+		id = EzShop.defineCustomer("Keanu Reeves");
+		assertTrue(id > 0);
+		
+		try {
+			cus1 = EzShop.getCustomer(id);
+		} catch (InvalidCustomerIdException e) {
+			fail();
+		}
+		
+		return;
+	}
 }
